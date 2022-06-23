bad53ca2c513c942a9a752794b121a2502bcbc19
Rodrigo Roli <rodrigo.monteiro.roli@gmail.com>
2022-06-23 17:26:30 -0300

    add callback3.js

---
+++ webmoderno/funcao/callback3.js
@@ -0,0 +1,6 @@
+// Exemplo de callback no browser
+document.getElementsByTagName('body')[0].onclick = function (e) {
+    console.log('O evento ocorreu!')
+}
+
+// joga isso no console do navegador para ver o callback acontecendo na prática
