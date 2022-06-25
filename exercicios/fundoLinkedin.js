9ddb1f9801f0b2c4e382181fe7e07fa92ece6159
Rodrigo Roli <rodrigo.monteiro.roli@gmail.com>
2022-06-25 19:42:02 -0300

    add fundoLinkedin.js

---
+++ exercicios/fundoLinkedin.js
@@ -0,0 +1,10 @@
+/* if you had one shot
+or one opportunity
+to seize everything you ever wanted
+one moment
+would you capture it, or just let it slip? */
+
+function opportunity(rightAttitude = 0) {
+    rightAttitude == 1 ? you = "everything" : you = 0
+    return you
+}
