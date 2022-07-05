603e9ecf3ec1b8a916cd812ea0bd58fdb6374c84
Rodrigo Roli <rodrigo.monteiro.roli@gmail.com>
2022-07-05 18:55:54 -0300

    04.js

---
+++ exercicios/curso/04.js
@@ -0,0 +1,10 @@
+/* 
+04) Crie uma função que irá receber dois valores, o dividendo e o divisor. A função deverá
+imprimir o resultado e o resto da divisão destes dois valores.
+*/
+
+function calc(dividendo, divisor) {
+    return arr = [dividendo / divisor, dividendo % divisor]
+}
+
+console.log(calc(15, 4))
