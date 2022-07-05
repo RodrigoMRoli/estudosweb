115eb0617f4ab9b00673cd47114bd46eea9dbb67
Rodrigo Roli <rodrigo.monteiro.roli@gmail.com>
2022-07-05 18:55:39 -0300

    add 01.js

--- exercicios/curso/01.js
+++ exercicios/curso/01.js
@@ -2,3 +2,13 @@
 1- Crie uma função que dado dois valores (passados como parâmetros) mostre no console a soma,
 subtração, multiplicação e divisão desses valores
 */
+function calc(a, b){
+    w = a + b
+    x = a - b
+    y = a * b
+    z = a / b
+    arr = [w, x, y, z]
+    return arr.toString()
+}
+
+console.log(calc(2, 2))
