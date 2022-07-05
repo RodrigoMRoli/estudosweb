3c2adbd376c48c46038a9a993509660ccc4e0880
Rodrigo Roli <rodrigo.monteiro.roli@gmail.com>
2022-07-05 18:55:50 -0300

    add 03.js

---
+++ exercicios/curso/03.js
@@ -0,0 +1,10 @@
+/*
+03) Crie uma função que recebe dois parâmetros, base e expoente,
+e retorne a base elevada ao expoente.
+*/
+
+function calc(base, expoente){
+    return Math.pow(base, expoente)
+}
+
+console.log(calc(3, 2))
